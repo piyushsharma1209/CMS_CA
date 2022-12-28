cd619d0d6f7da6eabfd013500296688916cdec2d
piyushsharma1209 <piyush.sharma@stud.noroff.no>
2022-12-28 04:04:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// main.js
+
+// This function makes a call to the WordPress REST API to retrieve the latest posts
+async function getPosts() {
+    const response = await fetch('http://piyushsharma.one/fedup/wp-json/wp/v2/posts');
+    const posts = await response.json();
+    return posts;
+}
+
+// This function takes a post object and creates an HTML element for it
+function createPostElement(post) {
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.addEventListener('click', () => {
+        window.location.href = `post.html?id=${post.id}`;
+    });
+
+    const titleElement = document.createElement('div');
+    titleElement.classList.add('post-title');
+    titleElement.innerHTML = post.title.rendered;
+    postElement.appendChild(titleElement);
+
+    return postElement;
+}
+
+// This function adds the posts titles to the page
+async function displayPosts() {
+    const posts = await getPosts();
+    const appElement = document.getElementById('app');
+    for (const post of posts) {
+        const postElement = createPostElement(post);
+        appElement.appendChild(postElement);
+    }
+}
+
+displayPosts();
